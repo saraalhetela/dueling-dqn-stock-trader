f0d93c53a8d898b1941a6af702fde05c6a68e8ab
saraalhetela <alhetelasara@gmail.com>
2025-08-15 18:17:33 +0300

    Update memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -1,1 +1,18 @@
+import random
+from collections import deque, namedtuple
+
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'done'))
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.memory = deque(maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
