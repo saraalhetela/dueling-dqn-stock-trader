8db5afe111153acb915d475c6ae205384eadefc8
saraalhetela <alhetelasara@gmail.com>
2025-08-15 19:06:25 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,23 +1,44 @@
+import os
 import pandas as pd
-from config import Config
+import matplotlib.pyplot as plt
 from environment.stock_env import StockEnv
 from agent.dqn_agent import DQNAgent
+from config import Config
 
-data = pd.read_csv("data/stock_prices.csv")
+# Ensure plots directory exists
+os.makedirs("plots", exist_ok=True)
+
+# Load stock data
+data = pd.read_csv(Config.STOCK_CSV)
 env = StockEnv(data)
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
-agent = DQNAgent(state_dim, action_dim, Config)
+agent = DQNAgent(env.observation_space.shape[0], env.action_space.n, Config)
 
-for episode in range(Config.num_episodes):
+all_rewards = []
+
+for episode in range(Config.EPISODES):
     state = env.reset()
     done = False
-    while not done:
-        action = agent.select_action(state, epsilon=0.1)
-        next_state, reward, done, _ = env.step(action)
-        agent.memory.push(state, action, reward, next_state, done)
-        state = next_state
-        agent.optimize()
-    if episode % Config.target_update == 0:
-        agent.target_net.load_state_dict(agent.policy_net.state_dict())
+    total_reward = 0
 
+    while not done:
+        action = agent.select_action(state)
+        next_state, reward, done, _ = env.step(action)
+        agent.store_transition(state, action, reward, next_state, done)
+        agent.learn()
+        state = next_state
+        total_reward += reward
+
+    all_rewards.append(total_reward)
+    print(f"Episode {episode + 1}/{Config.EPISODES}, Reward: {total_reward}")
+
+# Save trained model
+os.makedirs("models", exist_ok=True)
+agent.save_model("models/dqn_model.pth")
+
+# Plot rewards
+plt.plot(all_rewards)
+plt.xlabel("Episode")
+plt.ylabel("Total Reward")
+plt.title("Training Rewards")
+plt.savefig("plots/rewards.png")
+plt.close()
