50520b1f30565ee99928f791d7735682d4c2589c
Sara Alhetela <alhetelasara@gmail.com>
2025-08-18 16:29:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 class Config:
     STOCK_CSV = "data/AAPL_data.csv"
+    batch_size = 64
+    memory_size = 10000
+    gamma = 0.99
+    learning_rate = 0.0001
+    epsilon = 1.
+    k=0
+
     
-    # Environment
     initial_balance = 10000
     max_steps = 200
-
-    # DQN
-    gamma = 0.99
-    lr = 1e-4
-    batch_size = 64
     target_update = 10
-    memory_size = 10000
     n_step = 3
-    epsilon = 0.1
-
-    # Training
     EPISODES = 500
+
+
+
