954ea1f8db656cc5e5e1c03e8e581466e4f82528
Sara Alhetela <alhetelasara@gmail.com>
2026-06-29 17:47:04 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,29 @@
-# model.py
 import torch
 import torch.nn as nn
 
 class DuelingConv1D(nn.Module):
-    def __init__(self, input_channels, output_size):
-        super(DuelingConv1D, self).__init__()
-        self.conv1 = nn.Conv1d(input_channels, 128, 5)
-        self.conv2 = nn.Conv1d(128, 128, 5)
-        self.flatten = nn.Flatten()
-        self.state_value_linear1 = nn.Linear(5376, 512)
-        self.state_value_linear2 = nn.Linear(512, 1)
-        self.adv_linear1 = nn.Linear(5376, 512)
-        self.adv_linear2 = nn.Linear(512, output_size)
-        self.relu = nn.ReLU()
+    def __init__(self, input_channels, output_size, obs_bars=50):
+        super().__init__()
+        self.conv1 = nn.Conv1d(input_channels, 32, kernel_size=5)
+        self.conv2 = nn.Conv1d(32, 32, kernel_size=5)
+        self.relu  = nn.ReLU()
+        with torch.no_grad():
+            dummy = torch.zeros(1, input_channels, obs_bars)
+            flat_size = self._conv_out(dummy).shape[1]
+        self.value_fc1 = nn.Linear(flat_size, 128)
+        self.value_fc2 = nn.Linear(128, 1)
+        self.adv_fc1   = nn.Linear(flat_size, 128)
+        self.adv_fc2   = nn.Linear(128, output_size)
 
-    def forward(self, x):
+    def _conv_out(self, x):
         x = self.relu(self.conv1(x))
         x = self.relu(self.conv2(x))
-        x = self.flatten(x)
-        val = self.relu(self.state_value_linear1(x))
-        val = self.state_value_linear2(val)
-        adv = self.relu(self.adv_linear1(x))
-        adv = self.adv_linear2(adv)
+        return x.flatten(start_dim=1)
+
+    def forward(self, x):
+        f   = self._conv_out(x)
+        val = self.relu(self.value_fc1(f))
+        val = self.value_fc2(val)
+        adv = self.relu(self.adv_fc1(f))
+        adv = self.adv_fc2(adv)
         return val + adv - adv.mean(dim=1, keepdim=True)
