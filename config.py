7b2a520f0c35be358b7ae46337747b3cfcb5d5c2
Sara Alhetela <alhetelasara@gmail.com>
2026-06-29 17:45:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-# config.py
-
-DEVICE = "cuda"
-DATA_PATH = "./data/AAPL_data.csv"
+import torch
+DEVICE         = "cpu"
+DATA_PATH      = ".data/AAPL_data.csv"
 CHECKPOINT_DIR = "./checkpoints"
-OBS_BARS = 50
-BATCH_SIZE = 32
-GAMMA = 0.99
-LEARNING_RATE = 1e-4
-MEMORY_SIZE = 100000
-SYNC_FREQ = 1000
-MAX_STEPS = 150000
-EPSILON_START = 1.0
-EPSILON_END = 0.1
-N_STEP = 2
+OBS_BARS       = 50
+BATCH_SIZE     = 64
+GAMMA          = 0.99
+LEARNING_RATE  = 1e-4
+MEMORY_SIZE    = 20_000
+SYNC_FREQ      = 300
+MAX_STEPS      = 5_000
+EPSILON_START  = 1.0
+EPSILON_END    = 0.05
+N_STEP         = 2
+TRAIN_RATIO    = 0.70
+VAL_RATIO      = 0.15
